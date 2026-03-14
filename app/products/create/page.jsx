7b601485336a472e64c9840eaf2830f63099b359
Ronak <ronakrathod0022@gmail.com>
2026-03-14 16:54:37 +0530

    implement data fetching in ProductsCreateRoute and ProductsDetailsRoute

--- app/products/create/page.jsx
+++ app/products/create/page.jsx
@@ -1,5 +1,8 @@
-import ProductsCreatePage from '../../../components/products/products-create-page'
+import ProductsCreatePage, { productsCreatePageData } from '../../../components/products/products-create-page'
+import { fetchBackendResource } from '../../../lib/backend-api'
 
-export default function ProductsCreateRoute() {
-  return <ProductsCreatePage />
+export default async function ProductsCreateRoute() {
+  const pageData = await fetchBackendResource('/products/create', productsCreatePageData)
+
+  return <ProductsCreatePage pageData={pageData} />
 }
--- app/products/details/page.jsx
+++ app/products/details/page.jsx
@@ -1,5 +1,8 @@
-import ProductsDetailsPage from '../../../components/products/products-details-page'
+import ProductsDetailsPage, { productsDetailsPageData } from '../../../components/products/products-details-page'
+import { fetchBackendResource } from '../../../lib/backend-api'
 
-export default function ProductsDetailsRoute() {
-  return <ProductsDetailsPage />
+export default async function ProductsDetailsRoute() {
+  const pageData = await fetchBackendResource('/products/details', productsDetailsPageData)
+
+  return <ProductsDetailsPage pageData={pageData} />
 }
